7131d814bd0d85646f127100c771e754a6308d31
RaulMunozSaucedoJr <simkerboy_13@hotmail.com>
2021-06-03 16:12:11 -0500

    Cambios

--- Assets/TEMPLATES/HEAD/Head.php
+++ Assets/TEMPLATES/HEAD/Head.php
@@ -2,11 +2,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="Description" content="Enter your description here" />
+    <meta name="theme-color" content="#26C6DA">
+    <meta name="Description" content="Reclutamiento en Lion-Digital" />
+    <!-- BOOTSTRAP ASSET´S -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <!-- NORMALIZE ONLINE & LOCAL -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+        integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+        crossorigin="anonymous" />
     <link rel="stylesheet" href="/Assets/CSS/Normalize.css">
+    <!-- BOXICONS & FONTAWESOME -->
+    <link href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- HOJA DE ESTILOS PERSONAL -->
     <link rel="stylesheet" href="/Assets/SCSS/estilos.css">
+    <!-- ICONO TAB-NAVEGADOR -->
     <link rel="shortcut icon" href="/Assets/ICO/IconoLionDigital.ico" />
+
     <title>Talento - Lion Digital</title>
 </head>
--- Assets/VIEW/Recuperar__contrasena.php
+++ Assets/VIEW/Recuperar__contrasena.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<!-- HEAD -->
+<!-- HEAD-->
 <?php
     include_once '../TEMPLATES/HEAD/Head.php'
 ?>
 
 <body>
 
-    <!--NAVBAR VERSIÓN ESCRITORIO -->
+    <!-- NAVBAR VERSIÓN ESCRITORIO -->
     <?php
         include_once '../TEMPLATES/NAVBAR__ESCRITORIO/Navbar__escritorio.php'
     ?>
@@ -18,21 +17,14 @@
         include_once '../TEMPLATES/NAVBAR__MOVIL/Navbar__movil.php'
     ?>
 
-
-    <div class="container-fluid">
+    <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 pt-4 mb-4">
-                <?php
-                    include_once '../TEMPLATES/FORMULARIO__REGISTRO__ASPIRANTES/Formulario__registro__aspirantes.php'
-                ?>
-            </div>
+            <!-- FORMULARIO RECUPERAR CONTRASEÑA -->
+            <?php
+            include_once '../TEMPLATES/FORMULARIO__RECUPERAR__CONTRASENA/Formulario__recuperar__contrasena.php'
+        ?>
         </div>
     </div>
-
-    <!-- FOOTER -->
-    <?php
-        include_once '../TEMPLATES/FOOTER/Footer.php'
-    ?>
 
     <!-- RECURSOS FOOTER: BOOTSTRAP, JS, CDN EXTERNOS, ETC -->
     <?php
